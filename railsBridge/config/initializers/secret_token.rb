a1d2b1e96a612d23885bd58fef5bd53e24254dbb
Whien <sal95610@gmail.com>
2014-05-04 14:20:26 +0800

    UploadData

---
+++ railsBridge/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBridge::Application.config.secret_key_base = 'a9b17b687038faed390c8841179edd4b2a13441f61f79145de4b08d3d46a07967ef5fefcb4de326bd099d00bfdf66ea08b4ae988530c3b726cdd33c3d9335da7'
